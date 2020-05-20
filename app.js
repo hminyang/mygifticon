aefd8514ec5a13a4176a4f325715055b8d085e16
jiwoo Kang <wldn7238@naver.com>
2020-05-20 16:02:20 +0900

    payment

--- app.js
+++ app.js
@@ -50,14 +50,14 @@ app.listen(port, function(){
 //     next(createError(404));
 //   });
    
-  // 에러 핸들러
-  app.use(function(err, req, res, next) {
-    console.log('errr')
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // // 에러 핸들러
+  // app.use(function(err, req, res, next) {
+  //   console.log('errr')
+  //   // set locals, only providing error in development
+  //   res.locals.message = err.message;
+  //   res.locals.error = req.app.get('env') === 'development' ? err : {};
    
-    // render the error page
-    res.status(err.status || 500);
-    res.render('main');
-  });
+  //   // render the error page
+  //   res.status(err.status || 500);
+  //   res.render('main');
+  // });
