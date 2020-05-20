5d71a33b88e83f73d1de01fdfd85553e71b6cf06
HyeminYang <hminyang7@hanmail.net>
2020-05-20 17:17:52 +0900

    menu.ejs 수정

--- routes/menu.js
+++ routes/menu.js
@@ -4,9 +4,9 @@ var getConnection = require('../lib/db');
  
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //console.log("req.query",req.query); //
+  console.log("req.query",req.query); //
   
-  var store_id= req.query.store_id;
+  var store_id= req.query.store_key;
     getConnection((conn) => {
         var sql = "SELECT * FROM fintech.menu where store_key=?";
         conn.query(
@@ -19,7 +19,7 @@ router.get('/', function(req, res, next) {
             }
             else {
               //console.log(result[0].menu_key);
-              
+              console.log(result);
               res.render('menu',{data:result} )
             }
           }
